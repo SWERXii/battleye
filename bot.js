b24860f2a44d3d68bd4467596852b20922eab0fa
SWERXii <45774276+SWERXii@users.noreply.github.com>
2018-12-11 21:40:51 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,8 +13,8 @@ client.on('message', message => {
     if (sender.id === '521820814551678976') {
         return;
     }
-    if (msg.includes('NIGGA') || (msg.includes('NIGGER') || (msg.includes('NIBBA')))) {
-        message.channel.send(message.author + ' _has been banned due to tosic behaviour_')
+    if (msg.includes('NIGGA') || (msg.includes('NIGGER') || (msg.includes('NIBBA') || (msg.includes('IGGA'))))) {
+        message.channel.send(message.author + ' _has been banned due to tosic behaviour_ :be:')
     }
     if (msg.includes('NIGERIA'))
         message.author.send('Reporta2 al INADI')
