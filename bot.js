8ec5289920d69f281d21afd74870f18a14818663
SWERXii <45774276+SWERXii@users.noreply.github.com>
2018-12-11 22:23:09 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,9 +14,14 @@ client.on('message', message => {
         return;
     }
     if (msg.includes('NIGGA') || (msg.includes('NIGGER') || (msg.includes('NIBBA') || (msg.includes('IGGA'))))) {
-        message.channel.send(message.author + ' _has been banned due to tosic behaviour_')
+        message.channel.send(message.author + ' _has been banned due to tosic behaviour_ :be:')
     }
-    if (msg.includes('NIGERIA'))
-        message.author.send('Reporta2 al INADI')
+    if (msg.includes('NIGERIA')){
+        message.author.send('Reporta2 al INADI');
+    }
+    if (msg.includes('xs') || (msg.includes('es'))) {
+        message.delete();
+        message.channel.send(message.author + ' _acaba de escribir la palabra_ ' + message.content + ' _la cual contiene idioma inclusive que esta prohibido en este servxr._');
+    }
 });
 client.login(process.env.BOT_TOKEN);
