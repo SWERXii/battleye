3e61fb833ee16bfe8311ab0362d92f40dbcfa510
SWERXii <45774276+SWERXii@users.noreply.github.com>
2019-01-09 12:12:36 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,9 +12,13 @@ client.on('message', message => {
     let inclusive = ['xs', 'todes', 'elles', 'nosotres', 'bobes', 'divines', 'dibines', 'otres', 'chiques'];
     let foundInText = false;
 
-    client.user.setActivity("xvideos.com", {
-    type: "STREAMING",
-    url: "https://xvideos.com"
+    client.user.setStatus('available')
+    client.user.setPresence({
+        game: {
+            name: 'xvideos.com,
+            type: "STREAMING",
+            url: "https://xvideos.com"
+        }
     });
     for (var i in inclusive){
         if (message.content.toLowerCase().includes(inclusive[i].toLowerCase())) foundInText = true;
